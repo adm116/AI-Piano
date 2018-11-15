f6dadfec167d44455688f095f3ef8f8ae85328fc
adm116 <adm116@github.com>
2018-11-14 23:31:30 -0800

    update so works more with command line

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 import collections
 import tensorflow
 import numpy
@@ -11,9 +12,15 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import GRU
 from tensorflow.python.keras.callbacks import ModelCheckpoint
 from pathlib import Path
+
 LIMIT = 16 # limit number of files read in for now
-DATA_DIR = 'beethoven'
-EPOCHS = 50
+
+# specify directory of data, of pickle notes, of weights, num epocks, sequence length
+DATA_DIR = sys.argv[1]
+PICKLE_NOTES = sys.argv[2]
+WEIGHTS_PATH = sys.argv[3]
+NEW_PICKLE_NOTES = sys.argv[4]
+EPOCHS = int(sys.argv[5])
 SEQ_LEN = 100
 
 def getFiles():
@@ -24,72 +31,79 @@ def getFiles():
     return files
 
 def getNotes(files):
-    pickle_notes = Path("data/notes")
-    if pickle_notes.is_file():
-        with open('data/notes', 'rb') as filepath:
+    pickle_notes = Path(PICKLE_NOTES)
+    if NEW_PICKLE_NOTES == 'true' and pickle_notes.is_file():
+        print('Using previous notes')
+        with open(PICKLE_NOTES, 'rb') as filepath:
             return pickle.load(filepath)
 
     notes = []
     for i in range(0, LIMIT):
-    	file = files[i]
-    	midi = converter.parseFile(DATA_DIR + '/' + file)
-    	notes_to_parse = None
-    	parts = instrument.partitionByInstrument(midi)
-    	if parts: # file has instrument parts
-    		notes_to_parse = parts.parts[0].recurse()
-    	else: # file has notes in a flat structure
-    		notes_to_parse = midi.flat.notes
+        file = files[i]
+        midi = converter.parseFile(DATA_DIR + '/' + file)
+        notes_to_parse = None
+        try: # file has instrument parts
+            s2 = instrument.partitionByInstrument(midi)
+            notes_to_parse = s2.parts[0].recurse()
+        except: # file has notes in a flat structure
+            notes_to_parse = midi.flat.notes
 
-    	for element in notes_to_parse:
-            # single note
-    		if isinstance(element, note.Note):
-    			notes.append(str(element.pitch))
-            # chord
-    		elif isinstance(element, chord.Chord):
-    			notes.append('.'.join(str(n) for n in element.normalOrder))
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notes.append('.'.join(str(n) for n in element.normalOrder))
 
-    with open('data/notes', 'wb') as filepath:
+
+    with open(PICKLE_NOTES, 'wb') as filepath:
         pickle.dump(notes, filepath)
 
     return notes
 
 def getNetworkInputOuput(notes, n_vocab):
+    """ Prepare the sequences used by the Neural Network """
+    sequence_length = SEQ_LEN
+
     # get all pitch names
     pitchnames = sorted(set(item for item in notes))
-    # create a dictionary to map pitches to integers
-    note_to_int = dict((item, number) for number, item in enumerate(pitchnames))
+
+     # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
     network_input = []
     network_output = []
+
     # create input sequences and the corresponding outputs
-    for i in range(0, len(notes) - SEQ_LEN, 1):
-        sequence_in = notes[i:i + SEQ_LEN]
-        sequence_out = notes[i + SEQ_LEN]
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
         network_input.append([note_to_int[char] for char in sequence_in])
         network_output.append(note_to_int[sequence_out])
 
     n_patterns = len(network_input)
+
     # reshape the input into a format compatible with LSTM layers
-    network_input = numpy.reshape(network_input, (n_patterns, SEQ_LEN, 1))
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
     # normalize input
     network_input = network_input / float(n_vocab)
+
     network_output = utils.to_categorical(network_output)
+
     return (network_input, network_output)
 
 
 # Modeling
 def buildNetwork(network_input, n_vocab):
-    """
-    for the 1st layer (input tensor) we need to specify its shape (must have the same shape as training data)
-    shape is inferred for future layers.... THIS IS WHAT WE CAN TUNE (add/remove layers, tune params)
-    """
     model = Sequential() # linear stack of layers
     model.add(GRU(n_vocab, input_shape=(network_input.shape[1], network_input.shape[2]), activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     return model
 
 def trainModel(network_input, network_output, model):
-    filepath = "weights/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+    if not os.path.exists(WEIGHTS_PATH):
+        os.makedirs(WEIGHTS_PATH)
+
+    filepath = WEIGHTS_PATH + '/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5'
 
     # use checkpoints to save weights of the network nodes after every epoch
     checkpoint = ModelCheckpoint(
