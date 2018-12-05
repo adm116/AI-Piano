d27285c050d97bd6c4a8d94a35cb83dc2684847a
adm116 <adm116@github.com>
2018-12-04 23:44:46 -0800

    experiement

---
+++ parse_f.py
@@ -0,0 +1,100 @@
+import collections
+import os
+import glob
+import argparse
+import pickle
+import sys
+import numpy
+import matplotlib.pyplot as plt
+from pathlib import Path
+from music21 import converter, instrument, note, chord, stream, interval, pitch
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', help="a directory", type= str)
+parser.add_argument('--use_old_notes', help="whether or not to use old pickle notes", type= str, default='false')
+parser.add_argument('--limit', help="number of limited files to use", type= int, default=-1)
+parser.add_argument('--graph', help="whether to show a plot of frequencies", type= str, default='false')
+args = parser.parse_args(sys.argv[1:])
+
+DATA_DIR = args.dir
+USE_OLD_NOTES = args.use_old_notes.lower() == 'true'
+LIMIT = args.limit
+SHOW_GRAPH = args.graph.lower() == 'true'
+PICKLE_NOTES = DATA_DIR + '/notes'
+
+
+def getNotes():
+    pickle_notes = Path(PICKLE_NOTES)
+    if USE_OLD_NOTES and pickle_notes.is_file():
+        print('Using previous notes stored')
+        with open(PICKLE_NOTES, 'rb') as filepath:
+            return pickle.load(filepath)
+
+    files = []
+    for file in glob.glob(DATA_DIR + '/*.mid'):
+        files.append(file)
+
+    for file in glob.glob(DATA_DIR + '/*.midi'):
+        files.append(file)
+
+    if LIMIT != -1:
+        files = numpy.random.choice(files, LIMIT)
+
+    notes = []
+    for file in files:
+        midi = converter.parseFile(file)
+
+        # transpose
+        key = midi.analyze('key')
+        transposeKey = 'A' if key.mode == 'minor' else 'C'
+        i = interval.Interval(note.Note(key.tonicPitchNameWithCase), note.Note(transposeKey))
+        midi = midi.transpose(i)
+
+        notes_to_parse = None
+        parts = instrument.partitionByInstrument(midi)
+        if parts: # file has instrument parts
+        	notes_to_parse = parts.parts[0].recurse()
+        else: # file has notes in a flat structure
+        	notes_to_parse = midi.flat.notes
+
+        offsets = collections.defaultdict(list)
+        for element in notes_to_parse:
+            if isinstance(element, note.Note):
+                notes.append((str(element.pitch), element.duration.quarterLength, element.volume.velocity, element.offset))
+            elif isinstance(element, chord.Chord):
+                notes.append(('.'.join(sorted([str(note.Note(n).pitch) for n in element.normalOrder])), element.duration.quarterLength, element.volume.velocity, element.offset))
+
+    with open(PICKLE_NOTES, 'wb') as filepath:
+        pickle.dump(notes, filepath)
+
+    return notes
+
+def getFreq(notes):
+    pitchnames = sorted(set(item for item in notes))
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+    freq = collections.defaultdict(int)
+    for n in notes:
+        freq[note_to_int[n]] += 1
+    return freq
+
+def graph(notes):
+    freq = getFreq(notes)
+    y = []
+    for n in freq.keys():
+        y.append(freq[n])
+
+    plt.plot(sorted(y), '.')
+    plt.ylabel('number of occurences')
+    plt.xlabel('tokens sorted by freq')
+    plt.show()
+    print(sum(y))
+
+def parse():
+    print("Reading in notes...")
+    notes = getNotes()
+    if SHOW_GRAPH:
+        graph(notes)
+
+if __name__ == '__main__':
+    parse()
---
+++ train_f.py
@@ -0,0 +1,94 @@
+
+import os
+import sys
+import glob
+import collections
+import tensorflow
+import numpy
+import pickle
+import argparse
+from random import randint
+from music21 import converter, instrument, note, chord, stream
+from tensorflow.python.keras import utils
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import GRU
+from tensorflow.python.keras.callbacks import ModelCheckpoint
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dir', help="a directory", type= str)
+parser.add_argument('--epochs', help="number of epochs", type= int, default=200)
+parser.add_argument('--seq_len', help="length of sequences", type= int, default=100)
+parser.add_argument('--batch_size', help="batch size", type= int, default=128)
+args = parser.parse_args(sys.argv[1:])
+
+DATA_DIR = args.dir
+EPOCHS = args.epochs
+SEQ_LEN = args.seq_len
+BATCH = args.batch_size
+PICKLE_NOTES = DATA_DIR + '/notes'          # note file to put pickle info
+WEIGHTS_PATH = 'weights/' + DATA_DIR        # path for where to put weights
+
+def process(notes, n_vocab):
+    """ Prepare the sequences used by the Neural Network """
+    sequence_length = SEQ_LEN
+
+    # get all pitch names
+    pitchnames = sorted(set(item for item in notes))
+
+     # create a dictionary to map pitches to integers
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+    network_input = []
+    network_output = []
+
+    # create input sequences and the corresponding outputs
+    for i in range(0, len(notes) - sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        sequence_out = notes[i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+        network_output.append(note_to_int[sequence_out])
+
+    n_patterns = len(network_input)
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
+    network_input = network_input / float(n_vocab)
+    network_output = utils.to_categorical(network_output)
+    return (network_input, network_output)
+
+# Modeling
+def buildNetwork(network_input, n_vocab):
+    model = Sequential() # linear stack of layers
+    model.add(GRU(128, input_shape=(network_input.shape[1], network_input.shape[2]), activation='softmax', return_sequences=True))
+    model.add(GRU(n_vocab, input_shape=(network_input.shape[1], network_input.shape[2]), activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    return model
+
+def trainModel(network_input, network_output, model):
+    if not os.path.exists(WEIGHTS_PATH):
+        os.makedirs(WEIGHTS_PATH)
+
+    filepath = WEIGHTS_PATH + '/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5'
+
+    # use checkpoints to save weights of the network nodes after every epoch
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+    callbacks_list = [checkpoint]
+    model.fit(network_input, network_output, epochs=EPOCHS, batch_size=BATCH, callbacks=callbacks_list)
+
+def train():
+    with open(PICKLE_NOTES, 'rb') as filepath:
+        notes = pickle.load(filepath)
+
+    n_vocab = len(set(notes))
+    network_input, network_output = process(notes, n_vocab)
+    model = buildNetwork(network_input, n_vocab)
+    print("Training model...")
+    trainModel(network_input, network_output, model)
+
+if __name__ == '__main__':
+    train()
